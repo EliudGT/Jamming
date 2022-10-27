ed4e3ec88205c46b1cc13b3a604a92c670330613
oeliud8 <oeliud8@gmail.com>
2022-10-27 02:07:01 -0500

    uploading all the components

---
+++ src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './searchResults.css';
+
+import TrackList from "../TrackList/TrackList";
+
+class SearchResults extends React.Component {
+    render() {
+        
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/* <!-- Add a TrackList component --> */}
+                <TrackList tracks={this.props.searchResults}/>
+            </div>
+        )
+    }
+}
+
+export default SearchResults;
